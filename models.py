0ba7258bdb58126d47dce52f80e106b577bf24c1
jagan1238 <35834232+jagan1238@users.noreply.github.com>
2018-03-24 11:30:22 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from datetime import *
+class user(AbstractUser):
+    MALE=0
+    FEMALE=1
+    GENDER_CHOICES=((MALE,'male'),(FEMALE,'female'))
+    STUDENT=0
+    STAFF=1
+    DEPARTMENT=3
+    GUEST=4
+    VENDOR_EMPLOYESS=5
+    ACCOUNT_TYPE=(
+        (STUDENT,'student'),(STAFF,'staff'),(DEPARTMENT,'department' ),(GUEST,'guest')
+    )
+    uuid=models.CharField(max_length=500,null=True,blank=True)
+    account_type = models.IntegerField(blank=True, null=True, choices=ACCOUNT_TYPE)
+    dob=models.DateField(null=True,blank=True)
+    first_name = models.CharField(max_length=500,null=True,blank=True)
+    last_name = models.CharField(max_length=100,null=True,blank=True)
+    mobile_number=models.CharField(null=True,blank=True)
+    address=models.CharField(null=True,blank=True)
+    pin=models.IntegerField(null=True,blank=True)
+    email = models.EmailField(max_length=100)
+    blood_group = models.CharField(max_length=10)
+    religion = models.CharField(max_length=100)
+    age=models.CharField(dob.year-datetime.today().year)
+    profile_pic_url=models.CharField(max_length=5000,null=True,blank=True)
+
+class school(user):
+    MATRICULATION=0
+    STATE_BOARD=1
+    CBSE=2
+    ICSE=3
+    OTHER=4
+    SCHOOL_TYPE=(
+        (MATRICULATION,'Matriculation'),(STATE_BOARD,'State Board'),(CBSE,'CBSE'),(ICSE,'ICSE'),
+        (OTHER,"Other")
+    )
+    type=models.IntegerField(choices=SCHOOL_TYPE,default=OTHER)
+    id_number=models.CharField(max_length=1000,null=True,blank=True)
+
+class Teacher(user):
+    teacher_id=models.CharField(max_length=100,null=True,blank=True)
+    specialization=models.TextField()
+    resume_url=models.CharField(max_length=5000,null=True,blank=True)
+
+class Parent(user):
+    PARENT=0
+    GUARDIAN=1
+    PARENT_CHOICE=(
+        (PARENT,"Parent"),(GUARDIAN,"Guardian")
+    )
+class Student(user):
+    roll_no=models.CharField(max_length=1000,null=True,blank=True)
+    parent=models.OneToOneField(Parent,related_name='children_of')
+    addmission_no=models.CharField(max_length=1000,null=True,blank=True)
+
+
+
+
+
+
+
+
+
+
+
